23557c07787e23127dbeca13e842028862e92aa2
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-26 16:30:22 -0600

    remove debug statements

--- 2021/01.scala
+++ 2021/01.scala
@@ -4,15 +4,13 @@ object advent_of_code_2021_01 {
 	def main(args: Array[String]): Unit = {
 
 		val lines = Source.fromFile("01_input.csv").getLines().toList; // file as immutable list
-		var increases = 0;
+		var increases = 0; // initialize variable
 
 		for (i <- 1 until lines.length) { // iterate over list by index
 			if (lines(i).toInt > lines(i-1).toInt) { // evaluate
 				increases += 1; // increment when true
-				println(s"$i-${lines(i)}-${lines(i-1)}") // debug line
 			}
 		}
-
 		println(increases); // result
 	}
 }
