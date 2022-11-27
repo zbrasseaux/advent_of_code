676ece047561f0f7d2c7f9acc024cb2c9eb277de
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-11-26 18:17:11 -0600

    remove superfluous variables

--- 2021/02_1.scala
+++ 2021/02_1.scala
@@ -6,23 +6,21 @@ object advent_of_code_2021_02_1 {
 	def main(args: Array[String]): Unit = {
 
 		val lines = Source.fromFile("02_input.txt").getLines().toList; // file as immutable list
-		var forward: Int = 0; // initialize variable
-		var up: Int = 0; // initialize variable
-		var down: Int = 0; // initialize variable
+		var horizontal: Int = 0; // initialize variable
+		var vertical: Int = 0; // initialize variable
 
 		for (line <- lines) { // iterate over list by index
-			print(line);
 			var dir: String = line.split(" ")(0); // get direction
 			var dist: Int = line.split(" ")(1).toInt; // get distance
 
 			dir match { // switch - case
-				case "up" => {up += dist; println(s" $up")}
-				case "down" => {down += dist; println(s" $down")}
-				case "forward" => {forward += dist; println(s" $forward")}
+				case "up" => {vertical -= dist;}
+				case "down" => {vertical += dist;}
+				case "forward" => {horizontal += dist;}
 			}
 		}
 
-		print(s"($down - $up) * $forward = ");
-		println((down - up) * forward); // result
+		print(s"$vertical * $horizontal = ");
+		println(vertical * horizontal); // result
 	}
 }
