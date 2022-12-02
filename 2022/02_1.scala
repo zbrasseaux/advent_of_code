8d7acd10f1c79bbcacfefe8e7312ba71964165d6
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-12-02 10:05:41 -0600

    1st attempt on part 1

--- 2022/02_1.scala
+++ 2022/02_1.scala
@@ -1,27 +1,61 @@
 import scala.io.Source; // library to read input file in
 
-object advent_of_code_2022_01_1 {
+object advent_of_code_2022_02_1 {
 	def main(args: Array[String]): Unit = {
 
-		val lines = Source.fromFile("day_01.input").getLines().toList; // file as immutable list
+		// file as immutable list
+		val lines = Source.fromFile("day_02.input").getLines().toList;
+		// convert to actual
+		val rps_outcomes = Map(
+			"A" -> "rock", 
+			"B" -> "paper",
+			"C" -> "scissors",
+			"X" -> "rock", 
+			"Y" -> "paper",
+			"Z" -> "scissors"
+		);
+		// score modifiers
+		val rps_values = Map(
+			"rock" -> 1,
+			"paper" -> 2,
+			"scissors" -> 3
+		);
 		
 		// set default values
-		var currMax: Int = 0;
-		var currSum: Int = 0;
+		var currScore: Int = 0;
 
 		// iterate over file
 		for (line <- lines) {
-			if (line == "") { // line seperating elves
-				if (currSum > currMax) {
-					currMax = currSum; // new max
-				}
-				currSum = 0; // reset sum
+			val player_a: String = rps_outcomes(line.split(" ")(0));
+			val player_b: String = rps_outcomes(line.split(" ")(1));
+
+			currScore += rps_values(player_b) + parse_outcome(player_a, player_b);
+
+		println(currScore);
+	}
+
+	def parse_outcome(opp: String, player: String): Int -> {
+		if (opp == player) {
+			return 3;
+		} else if (opp == "rock") {
+			if (player == "paper") {
+				return 6;
 			} else {
-				currSum += line.toInt; // increment sum
+				return 0
+			}
+		} else if (opp == "paper") {
+			if (player == "scissors") {
+				return 6;
+			} else {
+				return 0
+			}
+		} else if (opp == "scissors") {
+			if (player == "rock") {
+				return 6;
+			} else {
+				return 0
 			}
 		}
-
-		println(currMax); // return result
 	}
 
 }
