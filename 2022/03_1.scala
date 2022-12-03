c9eda1be6e7e18d75e3183d8013ec3798b12fa06
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-12-03 13:53:03 -0600

    refactored how duplicates are detected

--- 2022/03_1.scala
+++ 2022/03_1.scala
@@ -13,15 +13,35 @@ object advent_of_code_2022_03_1 {
 			alphabet_map.put(alphabet(i), i+1);
 		}
 
+		var dup_items: Array[Char] = Array();
+
 		// iterate over file
 		for (line <- lines) {
-			for (x <- line) {
-				print(s"${alphabet_map(x)} ");
+			val half_one = line.slice(0, (line.length)/2);
+			val half_two = line.slice((line.length)/2, line.length-1);
+			var seen_elements: Array[Char] = Array();
+			println(s"${half_one.length} ${half_two.length}");
+
+			for (element_a <- half_one) {
+				if (!(seen_elements contains element_a)) {
+					for (element_b <- half_two) {
+						if (element_a == element_b) {
+							println(element_a);
+							dup_items = dup_items :+ element_a;
+						}
+					}
+					seen_elements = seen_elements :+ element_a;
+				}
 			}
-			println();
 		}
 
-		// println(currMax); // return result
+		var sum: Int = 0;
+
+		for (element <- dup_items) {
+			sum += alphabet_map(element)
+		}
+
+		println(sum); // return result
 	}
 
 }
