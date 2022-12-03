e4dcac1bbcc4cbbaba7d38fc0047f656630adeea
Zach Brasseaux <zbrasseaux97@gmail.com>
2022-12-03 14:04:44 -0600

    complete refactoring to use set logic

--- 2022/03_1.scala
+++ 2022/03_1.scala
@@ -20,18 +20,13 @@ object advent_of_code_2022_03_1 {
 			val half_one = line.slice(0, (line.length)/2);
 			val half_two = line.slice((line.length)/2, line.length-1);
 			var seen_elements: Array[Char] = Array();
-			println(s"${half_one.length} ${half_two.length}");
+			// println(s"${half_one.length} ${half_two.length}");
 
-			for (element_a <- half_one) {
-				if (!(seen_elements contains element_a)) {
-					for (element_b <- half_two) {
-						if (element_a == element_b) {
-							println(element_a);
-							dup_items = dup_items :+ element_a;
-						}
-					}
-					seen_elements = seen_elements :+ element_a;
-				}
+			val intersection = half_one.intersect(half_two);
+			println(intersection);
+
+			for (element <- intersection) {
+				dup_items = dup_items :+ element
 			}
 		}
 
